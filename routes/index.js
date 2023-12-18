6d38572d77fd46172273022ec6d30fbfca5b5491
joseftango <4279@holbertonschool.com>
2023-12-18 15:13:29 +0100

    index.js

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
+
 
 const router = (app) => {
   const paths = express.Router();
@@ -8,6 +10,7 @@ const router = (app) => {
 
   paths.get('/status', (request, response) => AppController.getStatus(request, response));
   paths.get('/stats', (request, response) => AppController.getStats(request, response));
+  paths.post('/users', (request, response) => UsersController.postNew(request, response));
 };
 
 export default router;
